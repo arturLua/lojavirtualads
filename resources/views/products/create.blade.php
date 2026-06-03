ddbbd53b9a8210f18fb9dfe3db1be627fc36e0a2
arturLua <pkllipsendereco@gmail.com>
2026-06-03 17:57:14 -0300

    feat: add image upload field to product creation form

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -1,14 +1,14 @@
 <x-app-layout>
 
-    <form class="w-full mx-auto max-w-md bg-white dark:bg-gray-800 p-6 rounded-lg shadow" action="{{ url('products/new') }}" method="POST">
+    <form class="w-full mx-auto max-w-md bg-white dark:bg-gray-800 p-6 rounded-lg shadow" action="{{ url('products/new') }}" method="POST" enctype="multipart/form-data">
         @csrf
 
         <h1 class="text-2xl font-bold mb-6 text-gray-900 dark:text-white">Cadastrar Produto</h1>
 
         @if($errors->any())
-        <div>
+        <div class="mb-4 p-3 bg-red-100 text-red-800 rounded">
             @foreach($errors->all() as $error)
-            {{ $error }}
+            <p>{{ $error }}</p>
             @endforeach
         </div>
         @endif
@@ -19,28 +19,29 @@
 
         <x-input-label class="block mb-1 text-gray-700 dark:text-gray-300">Descrição:</x-input-label>
         <x-text-input class="w-full p-2 mb-4 rounded border dark:bg-gray-700 dark:text-white" name="description" type="textarea" />
-        <x-input-error :messages="$errors->get('name')"/>
+        <x-input-error :messages="$errors->get('description')"/>
 
         <x-input-label class="block mb-1 text-gray-700 dark:text-gray-300">Quantidade:</x-input-label>
         <x-text-input class="w-full p-2 mb-4 rounded border dark:bg-gray-700 dark:text-white" name="quantity" type="number" />
-        <x-input-error :messages="$errors->get('name')"/>
+        <x-input-error :messages="$errors->get('quantity')"/>
 
         <x-input-label class="block mb-1 text-gray-700 dark:text-gray-300">Preço:</x-input-label>
         <x-text-input class="w-full p-2 mb-4 rounded border dark:bg-gray-700 dark:text-white" name="price" type="number" />
-        <x-input-error :messages="$errors->get('name')"/>
+        <x-input-error :messages="$errors->get('price')"/>
 
         <x-input-label class="block mb-1 text-gray-700 dark:text-gray-300">Tipo do produto:</x-input-label>
-
         <select class="w-full p-2 mb-4 rounded border dark:bg-gray-700 dark:text-white" name="type_id">
             <option value="">Selecione</option>
-
             @foreach($types as $type)
             <option value="{{ $type->id }}">
                 {{ $type->name }}
             </option>
             @endforeach
-
         </select>
+
+        <x-input-label class="block mb-1 text-gray-700 dark:text-gray-300">Imagem:</x-input-label>
+        <input class="w-full p-2 mb-4 rounded border dark:bg-gray-700 dark:text-white" name="image" type="file" accept="image/*" />
+        <x-input-error :messages="$errors->get('image')"/>
 
         <x-primary-button> Salvar </x-primary-button>
     </form>
